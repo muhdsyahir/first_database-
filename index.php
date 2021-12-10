e04fd2be6c4b8953c6e6f05c91b4c947266c1470
muhdsyahir <94956103+muhdsyahir@users.noreply.github.com>
2021-12-10 23:51:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<? php
-servename = "ec2-34-233-214-228.compute-1.amazonaws.com";
-$username = "ujecjnqievwhin";
-&password = "0f8b457958dd412343862aa5769864ce7ca29292816c0bb389c488488c23fef0";
-
-//creating connection
-$conn=new mysqli($servername, $username, $password);
-
-//checking connection
-if ($conn->connect_error)
-{
-	die ("connection failed : ".$conn->connect_error);
-}
-echo"connected successfully";
+<?php
+include_once 'includes/dbh.inc.php';
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,7 +8,7 @@ echo"connected successfully";
 <title>View</title>
 </head>
 <body>
-
+$conn;
 <h1><center>Mom's Baby World<center></h1>
 <form action="/action_page.php">
 <div style="background-color:#95afc0">
