939f8715113652e95a9081852839e2c0054e1ee6
muhdsyahir <94956103+muhdsyahir@users.noreply.github.com>
2021-12-11 00:15:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,22 +6,6 @@
 <title>View</title>
 </head>
 <body>
-	<?php
-$dbservename = "ec2-34-233-214-228.compute-1.amazonaws.com";
-$username = "ujecjnqievwhin";
-&password = "0f8b457958dd412343862aa5769864ce7ca29292816c0bb389c488488c23fef0";
-$dbname ="djf5r710q1slb";
-
-//creating connection
-$conn=new mysqli($servername, $username, $password,$dbname);
-
-//checking connection
-if ($conn->connect_error)
-{
-	die ("connection failed : ".$conn->connect_error);
-}
-echo"connected successfully";
-?>
 	<h1><center>Mom's Baby World<center></h1>
 </body>
 </html>
