f0d9cf12168bf596c66d60599253f9f6258a7aa4
muhdsyahir <94956103+muhdsyahir@users.noreply.github.com>
2021-12-11 00:46:35 +0800

    Create database.php

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'includes/connect.php';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      <?php
+      $sql="select * from SATFF;";
+      $result = mysqli_query($conn,$sql);
+      $resultcheck=mysqli_num_rows(result);
+      if ($resultcheck>0){
+      while($row=mysqli_fetch_assoc($result)){
+        echo $row['id'];
+      }
+      }
+      ?>
+    </title>
+  </head>
+</html>
