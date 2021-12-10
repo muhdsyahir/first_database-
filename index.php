73fd3103664752e25c17d3adea63ab9334fb1967
muhdsyahir <94956103+muhdsyahir@users.noreply.github.com>
2021-12-11 00:18:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<title>View</title>
-</head>
-<body>
-	<?php
+<?php
 $dbservename = "ec2-34-233-214-228.compute-1.amazonaws.com";
 $username = "ujecjnqievwhin";
 &password = "0f8b457958dd412343862aa5769864ce7ca29292816c0bb389c488488c23fef0";
@@ -22,6 +15,3 @@ if ($conn->connect_error)
 }
 echo"connected successfully";
 ?>
-	<h1><center>Mom's Baby World<center></h1>
-</body>
-</html>
