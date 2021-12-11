b740de7a596c0839dfb9405b4bbc2ac17ec83df5
muhdsyahir <94956103+muhdsyahir@users.noreply.github.com>
2021-12-11 14:44:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include_once 'connect.php';
   <body>
     <h1>hello</h1>
       <?php
-      $db="select * from SATFF;";
+      $url="select * from SATFF;";
       $result = mysqli_query($conn,$sql);
       $resultcheck=mysqli_num_rows(result);
       if ($resultcheck>0){
