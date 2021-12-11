a68ea8390cc41700726a4aa4504c0f911d1d34cb
muhdsyahir <94956103+muhdsyahir@users.noreply.github.com>
2021-12-11 14:51:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'connect.php';
+$url = parse_url(getenv("postgres://ujecjnqievwhin:0f8b457958dd412343862aa5769864ce7ca29292816c0bb389c488488c23fef0@ec2-34-233-214-228.compute-1.amazonaws.com:5432/djf5r710q1slb"));
 ?>
 <!DOCTYPE html>
 <html>
