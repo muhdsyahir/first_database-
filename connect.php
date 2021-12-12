b13abb2f82fb7a7a883daadd36d5b12421354068
DanialJafri <94578828+DanialJafri@users.noreply.github.com>
2021-12-12 15:20:19 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -13,7 +13,7 @@ if ($conn ->connect_error){
 else{
 	$stmt=$conn->prepare("insert into registration(prodID,prodSize,prodQty,prodName,prodDescription,prodPrice)
 values (?,?,?,?,?,?)");
-	$stmt->bind_param("iiissi",$prodID,$prodSize,$prodQty,$prodName,$prodDescription,$prodPrice);
+	$stmt->bind_param("iiissi",$prodid,$prodsize,$prodqty,$prodname,$proddescription,$prodprice);
 
 $stmt->execute();
 echo"add successful";
